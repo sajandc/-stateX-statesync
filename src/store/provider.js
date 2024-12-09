7f71f7a30077d9806710dd74ffea6877dc784bd0
Sajan Chouhan <sajan.chouhan@happay.in>
2024-12-09 17:28:45 +0530

    path fixed

--- src/store/provider.js
+++ src/store/provider.js
@@ -1,5 +1,5 @@
 import { Provider } from "react-redux";
-import store from "../store";
+import store from "./store";
 
 const StoreProvider = ({ children }) => {
   return <Provider store={store}>{children}</Provider>;
