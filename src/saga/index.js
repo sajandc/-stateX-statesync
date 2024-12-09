43148aa15915a346c997cdd360b1c70fa5c4cb19
Sajan Chouhan <sajan.chouhan@happay.in>
2024-12-09 17:31:00 +0530

    path fixed

--- src/saga/index.js
+++ src/saga/index.js
@@ -1,5 +1,5 @@
 import { all, fork } from "redux-saga/effects";
-import loginSaga from "./login.saga";
+import { loginSaga } from "./login.saga";
 
 export default function* rootSaga() {
   yield all([
